4cd58d7e0555a05ba99cbdfde28492d8b35248cf
Adam-McGill <37668746+Adam-McGill@users.noreply.github.com>
2022-03-20 13:27:55 -0500

    changes local modules name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "1.1.7" #"=0.14.2"
+  required_version = "1.1.7"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
@@ -13,7 +13,7 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "" # Input Sub to use.
+  subscription_id = "566bb512-2525-475b-9659-e9acdc19db11" # Input Sub to use.
   features {
     key_vault {
       purge_soft_delete_on_destroy    = false
@@ -21,3 +21,7 @@ provider "azurerm" {
     }
   }
 }
+
+#module "resource_group" {
+#  source = "https://github.com/Adam-McGill/terraform-aks/tree/main/Modules/resource_group"
+#}
