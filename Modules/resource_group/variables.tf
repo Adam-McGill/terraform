9694241b65c6c8b83cc82f9a66a71eede2c1c4da
Adam McGill <37668746+Adam-McGill@users.noreply.github.com>
2022-03-20 13:28:42 -0500

    Delete Modules/resource_group directory

--- Modules/resource_group/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {
-  description = "Azure region of the Resource Group"
-  type        = string
-}
-
-variable "name" {
-  description = "The name of the Resource Group"
-  type        = string
-}
